f4a1bbc6ad9154615fdbf622a5819279fa12ebd7
DevOpHBLICT <AppDevelopment@hblict.nhs.uk>
2017-10-13 11:39:39 +0100

    Tidying up android

--- pharm-dbb/pharm_dbb.Droid/MainActivity.cs
+++ pharm-dbb/pharm_dbb.Droid/MainActivity.cs
@@ -10,7 +10,7 @@ using SAFA;
 
 namespace pharm_dbb.Droid
 {
-    [Activity(Theme = "@android:style/Theme.Holo.Light", Label = "MCA", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Theme = "@android:style/Theme.Holo.Light", Label = "HSAB SAFA", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
     {
 
--- pharm-dbb/pharm_dbb.WinPhone/Properties/AssemblyInfo.cs
+++ pharm-dbb/pharm_dbb.WinPhone/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MCA")]
+[assembly: AssemblyTitle("HSAB SAFA")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MCA")]
+[assembly: AssemblyProduct("HSAB SAFA")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
